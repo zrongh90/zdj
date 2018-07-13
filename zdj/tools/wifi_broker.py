8b8cab4d99877663f744694fb5bbc7d20bd57128
zrongh90 <397864223@qq.com>
2018-07-13 22:33:49 +0800

    依赖包更新

--- zdj/tools/wifi_broker.py
+++ zdj/tools/wifi_broker.py
@@ -22,33 +22,6 @@ else:
     print(ifaces.status())
     print("password {0} is not collect".format(profile.key))
 
-# In [14]: profile.key="86525542"
-#
-# In [15]: ifaces.remove_all_network_profiles()
-#
-# In [16]: tmp_profile = ifaces.add_network_profile(profile)
-#
-# In [17]: ifaces.connect(tmp_profile)
-#
-# In [18]: ifaces.status()
-# Out[18]: 4
-#
-# In [19]: const.IFACE_CONNECTED
-# Out[19]: 4
-#
-# In [20]: ifaces.disconnect()
-#
-# In [21]: tmp_profile = ifaces.add_network_profile(profile)
-#
-#
-#
-
-
-
-
-
-
-
 
 
 
