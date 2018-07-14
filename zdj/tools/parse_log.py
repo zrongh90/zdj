d6a19d5197dd4834ecf6934622a9423d6a7937bc
zrongh90 <397864223@qq.com>
2018-07-14 21:10:32 +0800

    日志parse

--- zdj/tools/parse_log.py
+++ zdj/tools/parse_log.py
@@ -1,13 +1,16 @@
 # encoding: utf-8
-import collections
+import sys
 from datetime import datetime, timedelta
-ip_count = collections.defaultdict(int)
-with open('tools/access_log') as f:
+count_minutes = int(sys.argv[1])
+with open('/var/log/httpd/access_log') as f:
+    # current_time = datetime.now()
+    count = 0
+    current_time = datetime.strptime('2018-07-11 14:35:34', '%Y-%m-%d %H:%M:%S')
     for line in f:
         split_line = line.split()
-        current_time = datetime.strptime('2018-07-11 14:35:34', '%Y-%m-%d %H:%M:%S')
-        inside_one_minute = current_time - timedelta(minutes=1)
-        print(datetime.strptime(split_line[3][1:], '%d/%b/%Y:%H:%M:%S'))
+        inside_one_minute = current_time - timedelta(minutes=count_minutes)
+        # print(datetime.strptime(split_line[3][1:], '%d/%b/%Y:%H:%M:%S'))
         if datetime.strptime(split_line[3][1:], '%d/%b/%Y:%H:%M:%S') > inside_one_minute:
-            ip_count[split_line[0]] += 1
-    print(ip_count)
+            count += 1
+            # ip_count[split_line[0]] += 1
+    print(count)
