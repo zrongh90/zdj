730c6dd021ef8e2ce43cd45191b3d03bf3824a0f
zrongh90 <397864223@qq.com>
2018-07-21 23:09:32 +0800

    配置文件

---
+++ zdj/tools/config_parse.py
@@ -0,0 +1,9 @@
+#!encoding:utf-8
+import configparser
+
+config = configparser.ConfigParser()
+config['UserInfo'] = {'username': 'huangzirong184', 'password': ''}
+config['modules'] = {'interval(minutes)': 5, 'alert': 'True',
+                     'down_script': 'True', 'script_path': r'D:\script'}
+with open('config.cnf', 'w') as conf_file:
+    config.write(conf_file)
