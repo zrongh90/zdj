f0ebc20133fba788d87c1d05736fbf52546bb37c
zrongh90 <397864223@qq.com>
2018-10-04 19:59:42 +0800

    flask restful 主机信息收集 1）添加flask-admin支持 2）admin部分自定义

--- flask_monitor/admin.py
+++ flask_monitor/admin.py
@@ -1,3 +1,21 @@
 # encoding: utf-8
 from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 admin = Admin(name='flask_monitor', template_mode='bootstrap3')
+from flask_monitor.database import db
+from flask_monitor.models.BaseModels import UserModel, LinuxServerModel, ServerStatusModel, WASServerModel
+
+
+class UserModelView(ModelView):
+    # 排除选定列
+    column_exclude_list = ['email']
+    # 选中可过滤列
+    column_filters = ['username']
+
+
+# 通过自定义的ModelView去控制admin的功能
+admin.add_view(UserModelView(UserModel, db.session))
+# 基础的ModelView的绑定方法
+admin.add_view(ModelView(LinuxServerModel, db.session))
+admin.add_view(ModelView(ServerStatusModel, db.session))
+admin.add_view(ModelView(WASServerModel, db.session))
